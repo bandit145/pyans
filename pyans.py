25bddb9f9b69d3d3c48c6e4ecd3007c2fc71fc90
Philip Bove <pgbson@gmail.com>
2016-09-28 22:12:00 -0400

    fixed inventory pull from sensu

--- pyans.py
+++ pyans.py
@@ -1,15 +1,19 @@
-from paramiko import SSHClient
+#!/usr/bin/env python3
+#TODO: add error handling
+import paramiko
 from config import *
 import sys
 import http.client
 import json
+import getpass
 
 def begin():
 	print('Initialize connection to Ansible server...')
-	pkey_pass= input('Enter pkey pass > ')
-	ssh = SSHClient()
+	pkey_pass= getpass.getpass('Enter pkey pass > ')
+	ssh = paramiko.SSHClient()
+	key = paramiko.RSAKey(filename=priv_key_file, password=pkey_pass)
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(ans_server, password=pkey_pass, pkey=priv_key_file)
+	ssh.connect(ans_server, password=pkey_pass, pkey=key)
 	print(menu)
 	choice = input('> ')
 	if choice == '1':
@@ -26,7 +30,7 @@ def run_ans(ssh): #This needs to be split up playbook... maybe
 		playbook = input('Enter playbook name > ')
 		computer = input('Enter address of target > ')
 		pcname = input('Enter the desired name of the target machine > ')
-		stdin, stdout, stderr= ssh.exec_command('ansible-playbook {playbook} -i {hosts}, --extra-vars "host_name={name}" --ask-become-pass'.format(playbook=playbook, hosts=computer, name=pcname))
+		stdin, stdout, stderr= ssh.exec_command('ansible-playbook {playbook} -i {hosts}, --extra-vars "host_name={name}" --ask-become-pass --private-key {pkey}'.format(playbook=playbook, hosts=computer, name=pcname, pkey=pkey_location))
 		stdin.write(password)
 		stdin.flush()
 		print(stdout.rstrip('\n'))
@@ -38,10 +42,7 @@ def run_ans(ssh): #This needs to be split up playbook... maybe
 
 def list_plays(ssh):
 	stdin, stdout, stderr = ssh.exec_command('ls *.yml *.yaml')
-	if (str(stderr.read())) > 5:
-		print('No playbooks detected...')
-	else:
-		print(str(stdout))
+	print(stdout.read())
 	begin()
 
 
@@ -49,8 +50,9 @@ def get_inventory():#replace with requests
 	sensu = http.client.HTTPConnection(monitoring_location)
 	sensu.request('GET','/clients')
 	clients = sensu.getresponse()
-	clients = json.loads(clients.read(len(http_response_str)))
-	print(clients['name'])
+	clients = json.loads(clients.readall().decode('utf-8'))
+	for client in clients:
+		print(client['name']+'  '+ client['address'])
 	begin()
 
 #might turn into
