ee823794a9d9b0a0664358bdb01a73d5d038420d
bandit145 <pgbson@gmail.com>
2016-12-06 00:05:48 -0500

    using improved vmdeploy.ps1 script from "Scripts" repo

--- vmdeploy.ps1
+++ vmdeploy.ps1
@@ -6,29 +6,45 @@ param(
     [string]$template,
     [parameter(Mandatory=$true)]
     [string]$vmname
-
     )
-$ram = New-Object System.Collections.ArrayList 
-$vmhost = @{}
+#for hostname and loadvg
+$hostname = New-Object System.Collections.Specialized.OrderedDictionary
+#for the broken down cpu percentage and ram usage percentage
+$hostinfo = @{}
 Import-Module VMWare.VimAutomation.Core -ErrorAction "SilentlyContinue"
 Import-Module PowerCLI.ViCore -ErrorAction "SilentlyContinue"
 $credential = Get-Credential
 Connect-VIServer -Server $server -Credential $credential
 $hosts = Get-VMHost
-#make vmhost hash correspond to opem memeory and add open memeory to its own list
+#make vmhost hash table correspond to open memeory and add open memeory to its own list
 foreach($box in $hosts){
-    $sub = $box.MemoryTotalGB - $box.MemoryUsageGB
-    $vmhost.Add($sub, $box.Name)
-    $ram.Add($sub)
+    $ramper = $box.MemoryUsageGB / $box.MemoryTotalGB
+    $cpuper = $box.CpuUsageMhz / $box.CpuTotalMhz
+    $loadavg = ($ramper + $cpuper) / 2
+    #hostname = @{hostname = $loadavg}
+    $hostname.Add($box.name,$loadavg)
+    #hostinfo = {hostname = @{cpuper = .50; ramper = .40}; etc.}
+    $hostinfo.Add($box.name,@{cpuper = $cpuper; ramper = $ramper})
 }
-$ram = $ram | Sort-Object -Descending #sort memory open from largest amount to smallest amount
+$hostname = $hostname | Sort-Object -Property Value -Descending #sort hostname hash table by lowest load avg first
 #loop through ram arraylist and try to deploy to hosts
-foreach($num in $ram){
-    New-VM -VMHost $vmhost.$num -Template $template -Name $vmname -ErrorAction "SilentlyContinue" | Wait-Task
-    Start-VM -VM $vmname -ErrorAction "SilentlyContinue"
-    Start-Sleep -s 20
-    if($data = Get-VMGuest -VM $vmname -ErrorAction "SilentlyContinue"){
+foreach($vmhost in $hostname.Keys){
+    if($hostinfo.$vmhost.cpuper -lt .80 -Or $hostinfo.$vmhost.ramper -lt .75){ 
+        New-VM -VMHost $vmhost -Template $template -Name $vmname  
+        #do until stopgap since it seems wait-task is broken in newest powercli 6.5
+        do{
+            Write-Host "Creating "$vmname"...."
+             Start-Sleep -sec 5}
+        until(Get-VMGuest -VM $vmname)
+        Start-VM -VM $vmname -ErrorAction "SilentlyContinue"
+        do{
+            Write-Host "Wating for "$vmname"s ip ...."
+            Start-Sleep -sec 5
+            $data = Get-VMGuest -VM $vmname
+        }
+        until($data.IPAddress -like "192.168.*")#ip address to wait for (or this or that etc.)
         Write-Host $vmname ip address is $data.IPAddress
         exit
     }
+
 }
